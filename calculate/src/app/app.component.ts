174734cf57da42621a88a46ef6a30a2c13250bf8
hishamaman068-rgb <hishamaman068@gmail.com>
2026-02-21 16:53:54 +0530

    new

---
+++ calculate/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'calculate';
+  num1:number=0;
+  num2: number=0;
+  result:number=0;
+
+  calculate(){
+    this.result=this.num1+this.num2
+  }
+ 
+}
+
